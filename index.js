efe1f869797b5d2791c4ff824365ce888e57b700
Nadia-Kadri <nadiagrace96@gmail.com>
2024-01-06 17:20:35 -0500

    body-parser middleware

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.render("index.ejs")
 });
