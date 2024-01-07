b397794eb77265f5a539e096a77bdb39b0936f1f
Nadia-Kadri <nadiagrace96@gmail.com>
2024-01-07 15:23:17 -0500

    header, get routes for breakfast, dinner & dessert

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.get("/", (req, res) => {
   res.render("index.ejs")
 });
 
+app.get("/breakfast", (req, res) => {
+  res.render("breakfast.ejs")
+});
+
+app.get("/dinner", (req, res) => {
+  res.render("dinner.ejs")
+});
+
+app.get("/dessert", (req, res) => {
+  res.render("dessert.ejs")
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
